31d987299a62d20048a2a5b7d656410e778d069e
William Ryan <wcryan757@gmail.com>
2021-03-28 23:05:15 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(n) {1 + 80}
+
+function subtract(n) {60 - 40}
+
+function multiply(n) {2 * 3.4}
+
+function divide(n) {5.0 / 2.5}
+
+function add(a, b) {
+    return (a+b)
+}
+function subtract(a, b) {
+    return (a-b)
+}
+function multiply(a, b) {
+    return (a*b)
+}
+function divide(a, b) {
+    return (a/b)
+}
+
+function increment(n) {
+    return ++n;
+}
+
+function decrement(n) {
+    return --n;
+}
+
+function makeInt(n,){
+    return(parseInt(n,10))
+}
+
+function preserveDecimal(n){
+    return(parseFloat(n))
+}
+
+
+
+
